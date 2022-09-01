418868c48056eebe476bf03d376f643d9642dd4f
PagésSebastian <sebapages2@gmail.com>
2022-09-01 16:29:18 -0300

    Desafio 15 | Server Nginx

---
+++ Desafio 15 - Servidor Nginx/server.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express();
+const { Server: HttpServer } = require('http');
+const { Server: IOServer } = require('socket.io');
+const handlebars = require('express-handlebars');
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer)
+const apiContenedor = require('./utils/apiContenedor.js')
+const cookieParser = require('cookie-parser');
+const apiRoutes = require('./routes/apiRoutes')
+const session = require('express-session');
+const MongoStore = require("connect-mongo");
+const { engine } = require('express-handlebars');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require ("./config.js");
+
+const router = require('./routes/router');
+const os = require( "os");
+const cluster = require ("cluster");
+
+
+const cpus = os.cpus();
+
+app.engine(
+    "hbs",
+    handlebars.engine({
+        extname: ".hbs",
+        defaultLayout: 'main.hbs',
+    })
+    );
+    app.set('views', './views');
+    app.set('view engine', 'handlebars');
+
+    if (config.ARG.mode == "cluster" && cluster.isPrimary) {
+        cpus.map(() => {
+          cluster.fork();
+        });
+      
+        cluster.on("exit", (worker) => {
+          `Worker ${worker.process.pid} died!`;
+          cluster.fork();
+        });
+      } else {
+    
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }))
+    app.use(express.static('public'))
+    app.use(cookieParser());
+    app.use(session({
+      secret: config.KEY_SECRET,
+      rolling: true,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: false,
+        secure: false,
+        maxAge: config.TIEMPO_EXPIRACION,
+      },
+    }));
+    
+    app.use(passport.initialize());
+    app.use(passport.session());
+    
+    app.use(router);
+    app.use(express.static('views'));
+    app.engine('handlebars', engine());
+    app.use('/api', apiRoutes)
+
+
+const productos = apiContenedor.generarProducto()
+const mensajes = apiContenedor.generarMensajes()
+//Socket
+io.on('connection', (socket) => {
+    console.log('Un cliente se ha conectado')
+    socket.emit('messages', mensajes)
+    socket.emit('productos', productos)
+
+    socket.on('new-message', async data => {
+        apiContenedor.guardarMensaje(data)
+        io.sockets.emit('messages',  await apiContenedor.obtenerMensajes())
+    })
+
+    socket.on('new-product', data => {
+        io.sockets.emit('productos', apiContenedor.insertarProducto(data))
+    })
+})
+const server = httpServer.listen(config.ARG.port, async () => {
+    await mongoose.connect(config.MONGO_URL)
+    console.log(`Server running on port ${config.ARG.port} - pid: ${process.pid}`);
+});
+server.on('error', err => console.log(`Error: ${err}`));
+}
---
+++ Desafio 15 - Servidor Nginx/server2-2.js
@@ -0,0 +1,40 @@
+const express = require ("express");
+const router = require ("./routes/router");
+const { engine }= require ("express-handlebars");
+const config= require  ("./config.js");
+const os = require ("os");
+const cluster = require ("cluster");
+
+
+const app = express();
+const cpus = os.cpus();
+
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+    defaultLayout:"./views/layouts/main.handlebars",
+  })
+);
+app.set("view engine", "handlebars");
+app.set("views", "../public/views");
+
+
+if (config.ARG.mode == "cluster" && cluster.isPrimary) {
+  cpus.map(() => {
+    cluster.fork();
+  });
+
+  cluster.on("exit", (worker) => {
+    `Worker ${worker.process.pid} died!`;
+    cluster.fork();
+  });
+} else {
+  app.use("/api", router);
+
+  app.listen(config.ARG.port, (err) => {
+    err
+      ? console.log(`Se produjo un error al iniciar el servidor ${err}`)
+      : console.log(`El servidor esta escuchando el puerto ${config.ARG.port}`);
+  });
+}
