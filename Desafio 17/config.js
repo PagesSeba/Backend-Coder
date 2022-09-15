90f956de3caeb3efad5bf3423e1fe3af66018efa
PagésSebastian <sebapages2@gmail.com>
2022-09-15 15:45:30 -0300

    Desafio 17

---
+++ Desafio 17/config.js
@@ -0,0 +1,23 @@
+const dotenv = require("dotenv");
+const yargs  = require("yargs");
+
+dotenv.config();
+
+const yarg = yargs(process.argv.slice(2));
+const arg = yarg
+  .alias({
+    p: "port",
+    m: "mode",
+  })
+  .default({
+    port: 8080,
+    mode: "fork",
+  }).argv;
+
+module.exports = {
+  ARG: arg,
+  MONGO_URL: process.env.MONGO_URL,
+  KEY_SECRET: process.env.KEY_SECRET,
+  TIEMPO_EXPIRACION: parseInt(process.env.TIEMPO_EXPIRACION),
+  HOST: process.env.HOST,
+};
